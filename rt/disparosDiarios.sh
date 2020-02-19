32210d28d9120ae2f58d73f58abf99bd8e6ec3d8
evertonmessias <everton.messias@gmail.com>
2020-02-19 14:50:42 -0300

    19-02-2020_14:50:42

--- rt/disparosDiarios.sh
+++ rt/disparosDiarios.sh
@@ -17,7 +17,7 @@ teste="?dia=$1&mes=$2&ano=$3";
 else
 teste="";
 fi
-curl "$dominio/rt/rt_suporte1dia.php$teste";
-curl "$dominio/rt/rt_clientes1dia.php";
-curl "$dominio/rt/rt_clientes7dias.php";
+curl "$dominio/rt/sys/rt_suporte1dia.php$teste";
+curl "$dominio/rt/sys/rt_clientes1dia.php";
+curl "$dominio/rt/sys/rt_clientes7dias.php";
 
