3f1c694be49cd3c681059b520558fe252d7260c2
evertonmessias <everton.messias@gmail.com>
2020-02-21 09:43:07 -0300

    21-02-2020_09:43:07

--- rt/sys/rt_novo.php
+++ rt/sys/rt_novo.php
@@ -17,38 +17,44 @@ $cab_html = '<!DOCTYPE html>
 <html lang="pt-br">
 <head><meta charset="UTF-8" />
 <style>
-li{font-size:18px;}
-span{font-size:22px;font-weight:bold;}
+#texto{padding: 30px;font-family:Arial;
+display:block;position:relative;margin: 0 auto;
+width:600px;border: 1px solid #ccc;text-align:justify;
+}
 </style>
 </head><body>';
 
-$topo = "Prezado(a) Usuário(a), confirmamos sua Reserva de Sala conforme os dados descritos abaixo:<br>";
+$texto ="<h3>Prezado(a) Professor(a),</h3>
+<p>Você irá participar de uma Videoconferência em um(a) <b>".nomeTipo($tipo)."</b> no  Instituto
+  de  Computação, que será no dia " . $data . " as " . horaInicio($hora) . "h ("
+   . listaSalas($salas) ."). Utilizamos os softwares Skype ou Hangout (Google) para realizar 
+   a videoconferência, caso tenha  preferencia de  um outro software, por favor contatar via e-mail suporte@ic.unicamp.br assim que possível.</p>
+<p>Se necessitar agendar um teste antes da data prevista ou suporte com algum eventual
+ problema, estamos a disposição para ajudar, basta enviar um e-mail para suporte@ic.unicamp.br.</p>
+<p>** Garanta que seu sistema de áudio (microfone e saída de som) e webcam, estejam
+ funcionando no dia. Recomendados a utilização de um headset para uma melhor qualidade 
+ no áudio.</p>
+<p>Estamos a disposição desde já.</p>
+<p><b>Atenciosamente,</b><br>
+Equipe de TI do Instituto de Computação - Unicamp</p>
+</div>
+</body>
+</html>";
 
-$lista =
-  "<ul><li><strong>Inicio: </strong>" . $data . ", " . horaInicio($hora) . "h" .
-  "</li><li><strong>Sala: </strong>" . listaSalas($salas) .
-  "</li><li><strong>Tipo: </strong>" . nomeTipo($tipo) .
-  "</li><li><strong>Título: </strong>" . $titulo .
-  "</li><li><strong>Acessórios: </strong>" . nomeImportante($importante) .
-  "</li></ul>";
-
-$lembrete = "<br>Obs.:Chegar com pelo menos 30 min de antecedência<br>
-Dúvidas envie um e-mail para: <b>suporte@ic.unicamp.br</b> ou ligue: <b>35215915</b><br></body></html>";
-
-$mensagem = $cab_html . $topo . $lista . $lembrete;
+$mensagem = $cab_html . $texto;
 
 function enviar($destino, $mens)
 {
   $cabecalho = 'MIME-Version: 1.0' . "\r\n";
-  $cabecalho .= 'Content-type: text/html; charset=iso-8859-1;' . "\r\n";
-  $assunto = 'RESERVAS IC - Aviso';
+  $cabecalho .= 'Content-type: text/html; charset=UTF-8;' . "\r\n";
+  $assunto = 'Participação em Videoconferência, IC/Unicamp ';
   return mail($destino, $assunto, $mens, $cabecalho);
 }
 
 if ($tipo == 'J' || $tipo == 'D' || $importante == 1|| $importante == 4|| $importante == 5) {
   $ok = enviar($email, $mensagem);
   if ($ok) {
-    echo "Reserva criada: Um e-mail foi enviado para você !";
+    echo "Reserva criada; Um e-mail foi enviado!";
   } else {
     echo "MENSAGEM NÃO ENVIADA! ==> Título: $titulo, Data: $data, Hora de Inicio: " . horaInicio($hora) . ",
   Sala: " . listaSalas($salas) . ", Tipo: " . nomeTipo($tipo) . ", Email $email, Importante: " . nomeImportante($importante);
