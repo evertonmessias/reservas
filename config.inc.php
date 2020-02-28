bc57cf538dd48d7f5e97cc88f608d98ddc3d18bd
evertonmessias <everton.messias@gmail.com>
2020-02-28 16:33:56 -0300

    28-02-2020_16:33:56

--- config.inc.php
+++ config.inc.php
@@ -83,7 +83,7 @@ $mrbs_company_logo = "ic.gif";    // name of your logo file.   This example assu
 $mrbs_company_url = "http://www.ic.unicamp.br/";
 
 // ********** Variaveis RT ***********
-$suport_email = "everton.messias@gmail.com,ismael@ic.unicamp.br,pasergio@ic.unicamp.br,andre@ic.unicamp.br,william@ic.unicamp.br";
+$suport_email = "apoiovc@ic.unicamp.br";
 $dominio="http://localhost/reservas";
 
 $unicode_encoding = 1;
